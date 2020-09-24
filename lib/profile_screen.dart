a0712dbc3af1cee1b61b5f981af56bb30523b1dc
Telesphore <tuganimana01@gmail.com>
2020-09-24 19:47:59 +0200

    changed profile

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,5 +1,9 @@
+import 'package:SPENNapp/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:SPENNapp/signin_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -19,6 +23,25 @@ const List<Choice> choices = const <Choice>[
 ];
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  var userData;
+  void _getUserInfo() async {
+    SharedPreferences kubika = await SharedPreferences.getInstance();
+    var userJson = kubika.getString('user');
+    var user = json.decode(userJson);
+    var fullname = user['first_name'];
+
+    setState(() {
+      userData = user;
+    });
+  }
+
+  @override
+  void initState() {
+    _getUserInfo();
+
+    super.initState();
+  }
+
   void _select(Choice choice) {
     if (choice.title == 'Setting') {
       Navigator.pushNamed(context, '/settings');
@@ -76,7 +99,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
                 SizedBox(height: 15.0),
                 Text(
-                  'Telesphore',
+                  userData != null ? "${userData['first_name']} " : "",
                   style: TextStyle(
                       fontSize: 30.0,
                       fontWeight: FontWeight.bold,
@@ -92,42 +115,49 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         leading: Icon(Icons.phone_in_talk,
                             color: Theme.of(context).primaryColor),
                         title: Text(
-                          '+250784680568',
+                          userData != null ? "${userData['username']} " : "",
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               decoration: TextDecoration.underline),
                         ),
                         subtitle: Text('Tel. Number'),
                       ),
-                      ListTile(
-                        leading: Icon(Icons.email,
-                            color: Theme.of(context).primaryColor),
-                        title: Text('rindirobruce@gmail.com'),
-                        subtitle: Text('Email'),
-                      ),
+                      // ListTile(
+                      //   leading: Icon(Icons.email,
+                      //       color: Theme.of(context).primaryColor),
+                      //   title: Text('rindirobruce@gmail.com'),
+                      //   subtitle: Text('Email'),
+                      // ),
                       ListTile(
                         leading: Icon(Icons.location_on,
                             color: Theme.of(context).primaryColor),
-                        title: Text('Rindiro Bruce'),
+                        title: Text('Kigali'),
                         subtitle: Text('Location'),
                       ),
                     ],
                   ).toList(),
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: .0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        FontAwesome.sign_out,
-                        color: Colors.tealAccent,
-                      ),
-                      Text(
-                        'Signout',
-                        style: TextStyle(fontSize: 14.0),
-                      )
-                    ],
+                InkWell(
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 4.0, horizontal: .0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          FontAwesome.sign_out,
+                          color: Colors.tealAccent,
+                        ),
+                        Text(
+                          'Signout',
+                          style: TextStyle(fontSize: 14.0),
+                        )
+                      ],
+                    ),
                   ),
+                  onTap: () {
+                    Navigator.push(context,
+                        new MaterialPageRoute(builder: (context) => Login()));
+                  },
                 )
               ],
             ),
